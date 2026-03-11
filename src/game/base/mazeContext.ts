2bbf143dd9a2376241cb89e715ed6072b97e8ed3
Vicente Boluda Vias <vicente.boludavias@gmail.com>
2026-03-11 15:18:23 +0100

    threejs and mazeCotnext singleton

--- src/game/base/mazeContext.ts
+++ src/game/base/mazeContext.ts
@@ -1,14 +1,44 @@
 import { mazeCollisionService } from "./collision/mazeCollisionService";
 import { mazeEventBus } from "./eventBus/mazeEventBus";
+import { PerspectiveCamera, Scene } from "three";
+
+interface mazeContextDependencies {
+    mazeCollisionService: mazeCollisionService;
+    mazeEventBus: mazeEventBus;
+    scene: Scene;
+    camera: PerspectiveCamera;
+}
 
 export class mazeContext {
+    private static instance: mazeContext | null = null;
+
     private mazeCollisionService: mazeCollisionService;
     private mazeEventBus: mazeEventBus;
+    private scene: Scene;
+    private camera: PerspectiveCamera;
 
-    constructor() {
-        this.mazeCollisionService = new mazeCollisionService();
-        this.mazeEventBus = new mazeEventBus();
-    }  
+    private constructor(dependencies: mazeContextDependencies) {
+        this.mazeCollisionService = dependencies.mazeCollisionService;
+        this.mazeEventBus = dependencies.mazeEventBus;
+        this.scene = dependencies.scene;
+        this.camera = dependencies.camera;
+    }
+
+    static initialize(dependencies: mazeContextDependencies): void {
+        if (mazeContext.instance) {
+            throw new Error("mazeContext has already been initialized.");
+        }
+
+        mazeContext.instance = new mazeContext(dependencies);
+    }
+
+    static getInstance(): mazeContext {
+        if (!mazeContext.instance) {
+            throw new Error("mazeContext has not been initialized. Call initialize() first.");
+        }
+
+        return mazeContext.instance;
+    }
 
     getCollisionService(): mazeCollisionService {
         return this.mazeCollisionService;
@@ -16,6 +46,14 @@ export class mazeContext {
 
     getEventBus(): mazeEventBus {
         return this.mazeEventBus;
+    }
+
+    getScene(): Scene {
+        return this.scene;
+    }
+
+    getCamera(): PerspectiveCamera {
+        return this.camera;
     }
 };
 
